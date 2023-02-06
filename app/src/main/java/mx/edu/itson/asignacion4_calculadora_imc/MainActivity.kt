15f7010433679ed14db4ec5ea8e96d4e6c44a4ba
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-02-06 12:49:27 -0700

    rango con color

--- app/src/main/java/mx/edu/itson/asignacion4_calculadora_imc/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/asignacion4_calculadora_imc/MainActivity.kt
@@ -15,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     var peso: Double = 0.0
     var altura: Double = 0.0
     var new = false
+    var rango:String = "Rango"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,8 +39,27 @@ class MainActivity : AppCompatActivity() {
                 altura = height.text.toString().toDouble()
 
                 IMC = peso / altura.pow(2)
+                if(IMC<18.5){
+                    rango = "Bajo peso"
+                    tvRango.setBackgroundResource(R.color.colorGreenish)
+                }else if(IMC>=18.5 && IMC<=24.9){
+                    rango = "Normal"
+                    tvRango.setBackgroundResource(R.color.colorGreen)
+                }else if (IMC>=25 && IMC<=29.9){
+                    rango = "Sobrepeso"
+                    tvRango.setBackgroundResource(R.color.colorYellow)
+                }else if (IMC>=30 && IMC<=34.9){
+                    rango = "Obesidad grado 1"
+                    tvRango.setBackgroundResource(R.color.colorOrange)
+                } else if (IMC>=35 && IMC<=39.9){
+                    rango = "Obesidad grado 2"
+                    tvRango.setBackgroundResource(R.color.colorRed)
+                } else {
+                    rango = "Obesidad grado 3"
+                    tvRango.setBackgroundResource(R.color.colorBrown)
+                }
                 tvIMC.setText(String.format("%.1f", IMC))
-                tvRango.setText(rango(IMC))
+                tvRango.setText(rango)
 
             }
 
@@ -47,23 +67,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun rango (imc:Double): String {
-
-        if(imc<18.5){
-            return "Bajo peso"
-        }else if(imc>=18.5 && imc<=24.9){
-            return "Normal"
-        }else if (imc>=25 && imc<=29.9){
-            return "Sobrepeso"
-        }else if (imc>=30 && imc<=34.9){
-            return "Obesidad grado 1"
-        } else if (imc>=35 && imc<=39.9){
-            return "Obesidad grado 2"
-        } else {
-            return "Obesidad grado 3"
-        }
-
-    }
 
 
 }
