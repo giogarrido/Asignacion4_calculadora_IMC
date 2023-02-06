799e43b1f8a417ade1219984c802d5a319947ab9
Garrido <giovanni.garrido228724@potros.itson.edu.mx>
2023-02-06 12:10:57 -0700

    modificaciones y rango

--- app/src/main/java/mx/edu/itson/asignacion4_calculadora_imc/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/asignacion4_calculadora_imc/MainActivity.kt
@@ -22,27 +22,47 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val calcular: Button = findViewById(R.id.btnCalcular)
-        val tvIMC: TextView = findViewById(R.id.tvIMC)
-        val etPeso: EditText = findViewById(R.id.etPeso)
-        val etEstatura: EditText = findViewById(R.id.etEstatura)
+        val tvIMC: TextView = findViewById(R.id.imc)
+        val weight: EditText = findViewById(R.id.weight)
+        val height: EditText = findViewById(R.id.height)
+        val tvRango: TextView = findViewById(R.id.rango)
 
         //evento button clic o cliclisterer
         calcular.setOnClickListener {
 
 
-            if (etPeso.text.isEmpty() || etEstatura.text.isEmpty()) {
+            if (weight.text.isEmpty() || height.text.isEmpty()) {
                 tvIMC.setText("Ingrese un valor")
             } else {
-                peso = etPeso.text.toString().toDouble()
-                altura = etEstatura.text.toString().toDouble()
+                peso = weight.text.toString().toDouble()
+                altura = height.text.toString().toDouble()
 
                 IMC = peso / altura.pow(2)
-                tvIMC.setText(String.format("%.2f", IMC))
+                tvIMC.setText(String.format("%.1f", IMC))
+                tvRango.setText(rango(IMC))
 
             }
 
 
         }
+    }
+
+    fun rango (imc:Double): String {
+
+        if(imc<18.5){
+            return "Bajo peso"
+        }else if(imc>=18.5 && imc<=24.9){
+            return "Normal"
+        }else if (imc>=25 && imc<=29.9){
+            return "Sobrepeso"
+        }else if (imc>=30 && imc<=34.9){
+            return "Obesidad grado 1"
+        } else if (imc>=35 && imc<=39.9){
+            return "Obesidad grado 2"
+        } else {
+            return "Obesidad grado 3"
+        }
+
     }
 
 
